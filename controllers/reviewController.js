39801066e438aa613ce472a3a6967b10ced58e0c
Eng-Mohamed-Osama <eng.muhamed.osama@gmail.com>
2021-11-10 20:15:41 +0400

    adding the ability to delete reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -43,7 +43,60 @@ const addReview = async (req, res) => {
 
   res
     .status(StatusCodes.CREATED)
-    .json({ success: true, msg: "Thank you for submitting a review", product });
+    .json({ success: true, msg: "Thank you for submitting a review" });
 };
 
-module.exports = { addReview };
+const getProductReviews = async (req, res) => {
+  const { productID } = req.body;
+
+  if (!productID) {
+    throw new BadRequestError("please provide a valid product ID");
+  }
+
+  const product = await Product.findOne({ _id: productID });
+
+  if (!product) {
+    throw new NotFoundError("No product found");
+  }
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    reviews: product.reviews,
+  });
+};
+
+const deleteReview = async (req, res) => {
+  const { productID } = req.body;
+  const { _id: userID } = req.user;
+
+  if (!productID) {
+    throw new BadRequestError("please provide a valid product ID");
+  }
+
+  const product = await Product.findByIdAndUpdate(
+    { _id: productID },
+    { $pull: { reviews: { user: userID.toString() } } },
+    { new: true, runValidators: true, useFindAndModify: false }
+  );
+
+  if (!product) {
+    throw new NotFoundError("No product found");
+  }
+
+  if (product.reviews.length > 0) {
+    product.reviewAverage =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+  } else {
+    product.reviewAverage = 0;
+  }
+
+  await product.save({ validateBeforeSave: true });
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    product,
+  });
+};
+
+module.exports = { addReview, getProductReviews, deleteReview };
--- routes/review.js
+++ routes/review.js
@@ -4,8 +4,14 @@ const router = express.Router();
 
 const { authMiddleware } = require("../middlewares");
 
-const { addReview } = require("../controllers/reviewController");
+const {
+  addReview,
+  getProductReviews,
+  deleteReview,
+} = require("../controllers/reviewController");
 
 router.post("/add", authMiddleware, addReview);
+router.get("/getAllReviews", authMiddleware, getProductReviews);
+router.delete("/deleteReview", authMiddleware, deleteReview);
 
 module.exports = router;
